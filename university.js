90e2b929014b3e7b2bd6add534ffcc5f9707677e
YASSEROVIC <yasserovic@gmail.com>
2022-02-01 18:13:55 -0600

    Finishing the Assignment
    Do assignment, Working with DOM

---
+++ university.js
@@ -0,0 +1,19 @@
+class university {
+  constructor(
+    type,
+    established,
+    city,
+    studentsNumber,
+    color,
+    logo,
+  ) {
+  this.type = type;
+  this.established = established;
+  this.city = city;
+  this.studentsNumber = studentsNumber;
+  this.color = color;
+  this.logo = logo;
+  }
+}
+
+export default university;
